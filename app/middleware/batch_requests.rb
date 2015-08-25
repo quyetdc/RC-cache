2732a99c5485c08b0558676aaf12240809f998ed
quyetdc <quyetdc@evolableasia.vn>
2015-08-25 17:13:33 +0700

    Update rack for batch request

--- app/middleware/batch_requests.rb
+++ app/middleware/batch_requests.rb
@@ -5,15 +5,26 @@ class BatchRequests
 
   def call(env)
     if env["PATH_INFO"] == '/batch'
-      env["PATH_INFO"] = "/tasks.json"
-      env["QUERY_STRING"] = ""
-      env["REQUEST_METHOD"] = "GET"
-      env["rack.input"] = StringIO.new("")
-      @app.call(env)
-      # Inspect the environment hash to see what's in it
-      # [200, {"Content-Type" => "application/json"}, [env.inspect]]
+      request = Rack::Request.new(env.deep_dup)
+
+      responses = JSON.parse(request[:requests]).map do |override|
+        process_request(env.deep_dup, override)
+      end
+
+      [200, {"Content-type" => "application/json"}, [{responses: [responses]}.to_json]]
     else
       @app.call(env)
     end
+  end
+
+  def process_request(env, override)
+    path, query = override['url'].split('?')
+    env["REQUEST_METHOD"] = override['method']
+    env["PATH_INFO"] = path
+    env["QUERY_STRING"] = query
+    env["rack.input"] = StringIO.new(override["body"].to_s)
+    status, headers, body = @app.call(env)
+    body.close if body.respond_to? :close
+    {status: status, headers: headers, body: body.join}
   end
 end
